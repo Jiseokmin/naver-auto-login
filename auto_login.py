7dc8390a2eb4330648c633046535956890ae72b5
unknown <37jr@naver.com>
2023-11-17 15:30:40 +0900

    아이디, pw 입력 방식 복붙으로 변경

--- auto_login.py
+++ auto_login.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import pyperclip
 
 # 크롬드라이버 실행
 driver = webdriver.Chrome() 
@@ -18,16 +19,20 @@ move_login_page_button.click()
 time.sleep(2)
 
 # id 및 pw 입력
+user_id = 'ID를 입력하세요'
 id = driver.find_element(By .CSS_SELECTOR, "#id")
-id.send_keys('test')
+pyperclip.copy(user_id)
+id.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
 
+user_pw = '패스워드를 입력하세요'
 pw = driver.find_element(By .CSS_SELECTOR, "#pw")
-pw.send_keys('tset')
-
-time.sleep(2)
+pyperclip.copy(user_pw)
+pw.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
 
 # 네이버 로그인 페이지 이동 버튼 클릭
-login_button = driver.find_element(By .CSS_SELECTOR, ".btn_login")
+login_button = driver.find_element(by=By.ID, value ="log.login")
 login_button.click()
 
 time.sleep(2)
