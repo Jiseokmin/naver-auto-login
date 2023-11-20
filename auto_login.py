8607a62fe508c10e81edef120e4b2db5fe851fef
unknown <37jr@naver.com>
2023-11-20 15:17:58 +0900

    9222 포트로 연 크롬 제어할 수 있도록 변경

--- auto_login.py
+++ auto_login.py
@@ -1,38 +1,46 @@
 #환경 설정 세팅
 # selenium의 webdriver를 사용하기 위한 import
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 import pyperclip
+import webbrowser
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-# 크롬드라이버 실행
-driver = webdriver.Chrome() 
-driver.get('https://www.naver.com/')
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
-time.sleep(3)
+try :
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    driver.get("https://www.naver.com/")
 
-# 네이버 로그인 페이지 이동 버튼 클릭
-move_login_page_button = driver.find_element(By .CSS_SELECTOR, ".MyView-module__link_login___HpHMW")
-move_login_page_button.click()
+    # 네이버 로그인 페이지 이동 버튼 클릭
+    move_login_page_button = driver.find_element(By .CSS_SELECTOR, ".MyView-module__link_login___HpHMW")
+    move_login_page_button.click()
 
-time.sleep(2)
+    time.sleep(2)
 
-# id 및 pw 입력
-user_id = 'ID를 입력하세요'
-id = driver.find_element(By .CSS_SELECTOR, "#id")
-pyperclip.copy(user_id)
-id.send_keys(Keys.CONTROL, 'v')
-time.sleep(1)
+    # id 및 pw 입력
+    user_id = '아이디를 입력하세요'
+    id = driver.find_element(By .CSS_SELECTOR, "#id")
+    pyperclip.copy(user_id)
+    id.send_keys(Keys.CONTROL, 'v')
+    time.sleep(1)
 
-user_pw = '패스워드를 입력하세요'
-pw = driver.find_element(By .CSS_SELECTOR, "#pw")
-pyperclip.copy(user_pw)
-pw.send_keys(Keys.CONTROL, 'v')
-time.sleep(1)
+    user_pw = '비밀번호를 입력하세요'
+    pw = driver.find_element(By .CSS_SELECTOR, "#pw")
+    pyperclip.copy(user_pw)
+    pw.send_keys(Keys.CONTROL, 'v')
+    time.sleep(1)
 
-# 네이버 로그인 페이지 이동 버튼 클릭
-login_button = driver.find_element(by=By.ID, value ="log.login")
-login_button.click()
+    # 네이버 로그인 페이지 이동 버튼 클릭
+    login_button = driver.find_element(by=By.ID, value ="log.login")
+    login_button.click()
 
-time.sleep(2)
+    time.sleep(3)
+finally :
+    pass
